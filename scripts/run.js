b84c31ef76e1ca61b37f96d82c6bf2d91bff271b
[Faiz Kasman] <[faizkasman97@gmail.com]>
2022-04-21 18:14:24 +0800

    added // to run.js

--- scripts/run.js
+++ scripts/run.js
@@ -1,14 +1,16 @@
-const main = async () => {
-    const waveContractFactory = await hre.ethers.getContractFactory("WavePortal");
-    const waveContract = await waveContractFactory.deploy();
-    await waveContract.deployed();
-    console.log("Contract deployed to:", waveContract.address);
+//hre. doesnt have to be imported. running npx hardhat from terminal will auto build the hre object.
+
+const main = async () => {                                                            //async enables us to use await.
+    const waveContractFactory = await hre.ethers.getContractFactory("WavePortal"); //hre.ethers.getContractFactory("solidityfile.sol") compile contract
+    const waveContract = await waveContractFactory.deploy(); //deploying the contract into blockchain (locally in hardhat).
+    await waveContract.deployed(); //to wait until it's deployed first.
+    console.log("Contract deployed to:", waveContract.address); // this code will run after contract is deployed. waveContract.addreess will show the contract's address in the blockchain.
   };
   
   const runMain = async () => {
-    try {
+    try { //try catch statement to find error in const main.
       await main();
-      process.exit(0); // exit Node process without error
+      process.exit(0); // exit Node process without error // node.js statement.
     } catch (error) {
       console.log(error);
       process.exit(1); // exit Node process while indicating 'Uncaught Fatal Exception' error
